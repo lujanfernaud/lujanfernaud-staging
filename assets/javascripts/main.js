60bc5c637bae611426949581b6084aca41422e90
lujanfernaud <lujanfernaud@gmail.com>
2018-04-08 09:50:42 +0100

    Animate "Let's Talk" button when in viewport

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -77,19 +77,87 @@ const fader = {
   toggle:  function(element) { element.classList.toggle("opacity-hidden"); }
 };
 
-class contactFormToggler {
+class LetsTalkAnimator {
+
+  watch() {
+    let backCover    = document.querySelector(".back-cover");
+    let letsTalk     = new LetsTalkElementsToggler();
+    let pixelsOffset = { offset: -100 };
+
+    inViewport(backCover, pixelsOffset, animateLetsTalkWithDelay);
+
+    function animateLetsTalkWithDelay() {
+      window.setTimeout(() => {
+        letsTalk.toggleAnimation();
+        letsTalk.toggleHoverActive();
+      }, 1300);
+    }
+  }
+
+}
+
+class LetsTalkElementsToggler {
 
   constructor() {
+    this.letsTalk       = document.querySelector(".lets-talk-container");
+    this.letsTalkButton = document.querySelector(".lets-talk");
+    this.letsTalkText   = document.querySelector(".lets-talk__text");
+    this.letsTalkMail   = document.querySelector(".lets-talk__mail");
+
+    this.delayedToggler = new DelayedClassToggler();
+  }
+
+  toggle(formIsOpen = false) {
+    this.toggleAnimation();
+    this.toggleHoverActive(formIsOpen);
+    this.toggleContainer();
+  }
+
+  toggleAnimation() {
+    this.letsTalkText.classList.toggle("lets-talk__text--paused");
+    this.letsTalkMail.classList.toggle("lets-talk__mail--paused");
+  }
+
+  toggleHoverActive(formIsOpen = false) {
+    this.delayedToggler.toggle({
+      element:    this.letsTalkButton,
+      klass:      "lets-talk--hover-active",
+      formIsOpen: formIsOpen
+    });
+  }
+
+  toggleContainer() {
+    this.letsTalk.classList.toggle("lets-talk-container--hidden");
+  }
+
+}
+
+class DelayedClassToggler {
+
+  toggle({element, klass, formIsOpen = false}) {
+    if (formIsOpen) {
+      element.classList.toggle(klass);
+    } else {
+      window.setTimeout(() => {
+        element.classList.toggle(klass);
+      }, 1000); // 1 second, same as CSS transition duration.
+    }
+  }
+
+}
+
+class ContactFormToggler {
+
+  constructor() {
+    this.formIsOpen      = false;
     this.backCover       = document.querySelector(".back-cover");
-    this.letsTalk        = document.querySelector(".lets-talk-container");
-    this.letsTalkButton  = document.querySelector(".lets-talk");
-    this.letsTalkText    = document.querySelector(".lets-talk__text");
-    this.letsTalkMail    = document.querySelector(".lets-talk__mail");
     this.contactSection  = document.querySelector(".contact-section-container");
-    this.sectionOpen     = document.querySelector(".lets-talk-link");
-    this.sectionClose    = document.querySelector(".contact-section__close");
-    this.sectionTogglers = [this.sectionOpen, this.sectionClose];
-    this.showForm        = false;
+    this.sectionOpener   = document.querySelector(".lets-talk-link");
+    this.sectionCloser   = document.querySelector(".contact-section__close");
+    this.sectionTogglers = [this.sectionOpener, this.sectionCloser];
+
+    this.delayedToggler  = new DelayedClassToggler();
+    this.letsTalkToggler = new LetsTalkElementsToggler();
   }
 
   watch() {
@@ -108,29 +176,20 @@ class contactFormToggler {
   }
 
   toggleContactSection() {
-    this.showForm = !this.showForm; // Switch the boolean.
+    this.formIsOpen = !this.formIsOpen; // Switch the boolean.
 
     this.toggleVisibility();
     this.toggleScroll();
-    this.toggleButtonAnimation();
-    this.toggleButtonHoverActive();
-
     this.toggleLetsTalk();
     this.toggleContactForm();
   }
 
   toggleVisibility() {
-    this.toggleInAndOut(this.contactSection, "visibility-hidden");
-  }
-
-  toggleInAndOut(element, klass) {
-    if (this.showForm) {
-      element.classList.toggle(klass);
-    } else {
-      window.setTimeout(() => {
-        element.classList.toggle(klass);
-      }, 1000); // 1 second, same as CSS transition duration.
-    }
+    this.delayedToggler.toggle({
+      element:    this.contactSection,
+      klass:      "visibility-hidden",
+      formIsOpen: this.formIsOpen
+    });
   }
 
   toggleScroll() {
@@ -138,17 +197,8 @@ class contactFormToggler {
     this.backCover.classList.toggle("overflow-hidden");
   }
 
-  toggleButtonAnimation() {
-    this.letsTalkText.classList.toggle("lets-talk__text--paused");
-    this.letsTalkMail.classList.toggle("lets-talk__mail--paused");
-  }
-
-  toggleButtonHoverActive() {
-    this.toggleInAndOut(this.letsTalkButton, "lets-talk--hover-active");
-  }
-
   toggleLetsTalk() {
-    this.letsTalk.classList.toggle("lets-talk-container--hidden");
+    this.letsTalkToggler.toggle(this.formIsOpen);
   }
 
   toggleContactForm() {
@@ -159,4 +209,5 @@ class contactFormToggler {
 
 new NameFlipper().watch();
 new ChevronHider().watch();
-new contactFormToggler().watch();
+new LetsTalkAnimator().watch();
+new ContactFormToggler().watch();
