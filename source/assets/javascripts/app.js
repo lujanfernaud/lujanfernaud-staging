a5b22f7156451c75cf511261c3b21c0665acd170
lujanfernaud <lujanfernaud@gmail.com>
2018-09-21 06:48:17 +0100

    Move App 'requires' into app.js

--- source/assets/javascripts/app.js
+++ source/assets/javascripts/app.js
@@ -1,4 +1,12 @@
-// Main App File
+//= require helpers/fader
+//= require helpers/delayed-class-toggler
+
+//= require name-flipper
+//= require chevron-hider
+//= require text-selector
+//= require lets-talk-animator
+//= require contact-form-toggler
+//= require modal-toggler
 
 class App {
   run() {
--- source/assets/javascripts/main.js
+++ source/assets/javascripts/main.js
@@ -3,15 +3,5 @@
 //= require smooth-scroll.polyfills.min
 //= require tippy.min
 
-//= require helpers/fader
-//= require helpers/delayed-class-toggler
-
-//= require name-flipper
-//= require chevron-hider
-//= require text-selector
-//= require lets-talk-animator
-//= require contact-form-toggler
-//= require modal-toggler
-
 //= require app
 //= require tooltips
