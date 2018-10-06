b7fc733dad5b4023e3afbd2351f32a863ce814ca
lujanfernaud <lujanfernaud@gmail.com>
2018-10-06 07:57:01 +0100

    Add project_link_title helper method

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -2,4 +2,14 @@ module CustomHelpers
   def parameterize(title)
     title.gsub(",", " ").split.join("-").downcase
   end
+
+  def project_link_title(link)
+    title = link.title
+
+    if title == "Online"
+      "See #{title}"
+    else
+      "See on #{title}"
+    end
+  end
 end
