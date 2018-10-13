12fb17630863c8c463e0a0bed5fe551aaf45d1c7
lujanfernaud <lujanfernaud@gmail.com>
2018-10-13 14:41:49 +0100

    Remove unnecessary comments in config.rb

--- config.rb
+++ config.rb
@@ -1,8 +1,5 @@
 require 'sprockets/es6'
 
-# Activate and configure extensions
-# https://middlemanapp.com/advanced/configuration/#configuring-extensions
-
 activate :livereload
 
 activate :autoprefixer do |prefix|
@@ -23,42 +20,20 @@ activate :sprockets do |c|
   c.supported_output_extensions << '.es6'
 end
 
-set :css_dir, 'assets/stylesheets'
-set :fonts_dir, 'assets/fonts'
+# Asset folders
+
+set :css_dir,    'assets/stylesheets'
+set :fonts_dir,  'assets/fonts'
 set :images_dir, 'assets/images'
-set :js_dir, 'assets/javascripts'
+set :js_dir,     'assets/javascripts'
 
 # Layouts
 # https://middlemanapp.com/basics/layouts/
 
 # Per-page layout changes
-page '/*.xml', layout: false
+page '/*.xml',  layout: false
 page '/*.json', layout: false
-page '/*.txt', layout: false
-
-# With alternative layout
-# page '/path/to/file.html', layout: 'other_layout'
-
-# Proxy pages
-# https://middlemanapp.com/advanced/dynamic-pages/
-
-# proxy(
-#   '/this-page-has-no-template.html',
-#   '/template-file.html',
-#   locals: {
-#     which_fake_page: 'Rendering a fake page with a local variable'
-#   },
-# )
-
-# Helpers
-# Methods defined in the helpers block are available in templates
-# https://middlemanapp.com/basics/helper-methods/
-
-# helpers do
-#   def some_helper
-#     'Helping'
-#   end
-# end
+page '/*.txt',  layout: false
 
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
@@ -66,7 +41,7 @@ page '/*.txt', layout: false
 configure :build do
   activate :asset_hash
   activate :minify_html
-  activate :minify_css, inline: true
+  activate :minify_css,        inline: true
   activate :minify_javascript, inline: true
   activate :relative_assets
   activate :imageoptim
